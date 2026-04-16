8152d0dfa1251105eed9bcce4770a44a0823f6b2
twistedinferno <twistedinferno@hotmail.com>
2026-04-16 12:14:38 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "gcp_project" {}
+
+provider "google" {
+  project = var.gcp_project 
+  region  = "us-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state-student_03_c7e27b4e4d6b-10711-18220""
+    prefix  = "terraform/state/lab01" 
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
+}
